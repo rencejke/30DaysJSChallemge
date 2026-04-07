19064feb216466d4981bf8c4bc8c8d53353d5866
rencejke <bonillaclarencejake@gmail.com>
2026-04-08 02:05:49 +0800

    day 2 exercise 2 is still in progress

--- Day2/exercise2.js
+++ Day2/exercise2.js
@@ -68,7 +68,29 @@ console.log(randomNum) //0 - 100
 let randomNum50to100 =  Math.floor(Math.random() * 51) + 50 //50 to 100
 console.log(randomNum50to100)
 
-//9 ....
+//9
 
+let randomNum0to255 = Math.floor(Math.random() * 256) // -> 0 - 255
+console.log(randomNum0to255)
+
+//10
+let string = 'JavaScript'
+let randomNummmStr = Math.floor(Math.random() * string.length + 1) - 1 // 0 to 9
+console.lof(string[randomNummmStr])
+
+//11
+
+console.log('1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125') 
+/*
+
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+
+*/
+
+//12
 
 
