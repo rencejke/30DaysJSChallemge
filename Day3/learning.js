c35d567bc1780dd8b98c878c665462292cdc2d08
rencejke <bonillaclarencejake@gmail.com>
2026-04-08 23:13:06 +0800

    day 3 ongoing

---
+++ Day3/learning.js
@@ -0,0 +1,49 @@
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isMarried = true
+let truValue = 4 > 3    // true
+let falseValue = 4 < 3  // false
+
+/*  
+Truthy values
+
+All numbers(positive and negative) are truthy except zero
+All strings are truthy except an empty string ('')
+The boolean true
+
+*/
+
+/*
+Falsy values
+0
+0n
+null
+undefined
+NaN
+the boolean false
+'', "", ``, empty string
+
+*/
+
+
+//undefined
+let firstName
+console.log(firstName) //not defined, no assigned value
+
+let empty = null
+console.log(empty) //null, no value
+
+/*
+1. 
+Operator: = 
+example: x = y
+description: y is stored in x
+
+2.
+Operator: += 
+example: x = x + y
+description: x + y result is stored in x
+
+
+*/
