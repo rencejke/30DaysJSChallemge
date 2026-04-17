62049f851ae45da64f677fa02a47165ebf3bbaac
rencejke <bonillaclarencejake@gmail.com>
2026-04-18 00:57:35 +0800

    Day 5 ongoing

---
+++ Day5/learning.js
@@ -0,0 +1,25 @@
+//Arrays
+
+//How to create an empty array
+
+// - Using Array constructor
+const arr = Array()
+//or
+//let arr = new Array()
+console.log(arr)
+
+// - uing square brackers []
+
+const arr2 = []
+console.log(arr2)
+
+//How to create an array with values:
+
+const numbers = [0, 3.14, 6,7, 9.81, 37, 98.6, 100]
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+console.log('Numbers:', numbers) //Numbers: [0, 3.14, 9.81, 37, 98.6, 100]
