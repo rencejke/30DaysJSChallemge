354c78e549f04dbdc314d536ff2e93bfcd0628c4
rencejke <bonillaclarencejake@gmail.com>
2026-05-05 23:24:18 +0800

    day 7 still ongoing

--- Day7/learning.js
+++ Day7/learning.js
@@ -246,7 +246,150 @@ const anonymousFun = function() {
 
 anonymousFun()
 
-//Expression Function
+//9. Expression Function
+
+//Express functions are anonymous functions. After we create a function without a name and we assign it to a variable. 
+//To return a value from the function we should call the variable.
+
+const square = function(n) {
+    return n * n
+}
+
+console.log(square(10))
+
+//10. Self Invoiking Functions
+
+//Self invoking functions are anonymous functions which do not need to be called to return a value
+
+(function(n){
+    console.log(n * n)
+})(2) // 4, but instead of just printing if we want to return and store the data, we do as shown below
+
+let square = (function(n){
+    return n * n
+})(10)
+
+console.log(square) //100
+
+
+//11. Arrow Functions
+
+//Arrow function is an alternative to write a function, however function declaration and arrow function have some minor differences.
+//Arrow function uses arrow instead of the keyword function to declare a function
+
+// This is how we write normal or declaration function
+// Let us change this declaration function to an arrow function
+
+function square(n) {
+  return n * n
+}
+console.log(square(10))
+
+const square = n => { // parenthesis is optional if a single parameter
+
+return n * n    
+}
+console.log(square(10))
+
+const square = (n) =>{ //optional
+
+return n * n    
+}
+console.log(square(10))
+
+// if we have only one line in the code block, it can be written as follows, explicit return
+const square = n => n * n 
+
+console.log(square(10))  // -> 4
+
+//arrow function to change array to uppercase 
+const changeToUpperCase = arr =>
+{
+    let newArr = []
+    
+    for(const element of arr)
+    {
+        newArr.push(element.toUpperCase())
+    }
+    
+    return newArr
+}
+
+const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+console.log(changeToUpperCase(countries)) // ["FINLAND", "SWEDEN", "NORWAY", "DENMARK", "ICELAND"]
+
+const fullName = (firstName, lastName) =>{ //used parenthesis because i used more than one parameter
+    return `${firstName}, ${lastName}`
+}
+
+console.log(fullName('Clarence Jake', 'Bonilla'))
+
+//The above function has only the return statement, therefore, we can explicitly return it as follows.
+
+const printFullName = (firstName, lastName) => `${firstName} ${lastName}` 
+//if inside the function is only the return value then we can use this one line function
+
+console.log(printFullName('Clarence Jake', 'Bonilla'))
+
+
+//12. Functions with default parameters
+
+//If you don’t pass a value when calling the function, it will use the default instead.
+
+function functionName(param = value){
+ //code
+}
+
+// Calling function
+functionName()
+functionName(arg)
+
+//example
+
+function greetings(name = 'Clarence'){
+    let message = `${name}, welcome to 30 days of JavaScript`
+    return message
+}
+
+console.log(greetings()) //defvault value if no argument - > Clarence, welcome to 30 days of JavaScript
+console.log(greetings('Peter')) //Peter, welcome to 30 days of JavaScript
+
+function generateFullName (firstName = 'Clarence Jake', lastName = 'Bonilla')
+{
+    let space = ' '
+    let fullName = firstName + space + lastName
+    
+    return fullName
+}
+
+console.log(generateFullName()) //Clarence Jake Bonilla
+console.log(generateFullName('Peter', 'Pan')) //Peter Pan
+
+
+function calculateAge(birthYear, currentyear = 2026) {
+
+    let age = currentyear - birthYear
+
+    return age 
+}
+
+console.log('Age:', calculateAge(2002)) //24
+
+
+const weightOfObject = (mass, gravity = 9.81) => {
+
+    let weight = mass * gravity + ' N'  // the value has to be changed to string first
+
+    return weight;
+}
+
+console.log('Weight of an object in Newton: ', weightOfObject(100)) // gravity at the surface of Earth
+console.log('Weight of an object in Newton: ', weightOfObject(100, 1.62))  // gravity at surface of Moon
+
+
+
+
+
 
 
 
