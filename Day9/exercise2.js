f77d2dae759793d1fa872601960f19221a4f07a3
rencejke <bonillaclarencejake@gmail.com>
2026-08-08 19:51:21 +0800

    day 9 exercise level 2 done

--- Day9/exercise2.js
+++ Day9/exercise2.js
@@ -75,4 +75,107 @@ arr.map(country => country[0]).reduce((acc, curr, index) =>
     
 const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
 console.log(countNumberStarted(countries))
+
+
+//5 
+const countNumberStarted = arr => 
+    arr.map(country => country[0]).reduce((acc, curr) => {
+
+        // Check if the current letter already exists in the object.
+        // On the first occurrence of a letter, it is undefined,
+        // so we create the letter and set its count to 1.
+        // If the letter appears again, we increase its count by 1.
+
+        if (acc[curr] === undefined) {
+            acc[curr] = 1
+        } else {
+            acc[curr] += 1
+        }
+
+        return acc
+
+    }, {}) // acc starts as an empty object
+    
+    const countries = ['Finland', 'Sweden', 'France', 'Denmark', 'Norway', 'IceLand'] 
+    console.log(countNumberStarted(countries)) //{ F: 2, S: 1, D: 1, N: 1, I: 1 }
+
+
+//5 Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
+  
+// const getFirstTenCountries = arr =>
+//     arr.slice(0 , 10).map(country => country.toLowerCase()
+// )
+
+// const getFirstTenCountries = arr =>
+//     arr.slice(0 , 10).map(country => country[0]).reduce((acc, curr) =>
+//     {
+//         if (acc[curr] === undefined) {
+//             acc[curr] = 1
+//         } else {
+//             acc[curr] += 1
+//         }
+//         return acc
+//     }, {})
+
+// countries.slice(0, 10).forEach(country => console.log(country.toUpperCase()))
+
+const getFirstTenCountries = arr =>
+    arr.slice(0 , 10).map(country => country.slice(0, 3).toUpperCase()
+)
+
+import { countries } from './countries.js';
+
+//6 
+
+const getLastTenCountries = arr =>
+//arr.slice(arr.length - 10, arr.length).map(country => country.slice(0, 3).toUpperCase())
+arr.slice(arr.length - 10).map(country => country.slice(0, 3).toUpperCase())
+
+console.log(getLastTenCountries (countries))
+
+//7 Find out which letter is used many times as initial for a country name from the countries array (eg. Finland, Fiji, France etc)
+
+import { countries } from './countries.js';
+
+const countLargestInitial = arr =>
+arr.map(country => country[0]).reduce((acc, curr) =>
+{
+    if(acc[curr] === undefined)
+    {
+        acc[curr] = 1
+    }else
+    {
+        acc[curr] += 1
+    }
+    
+    return acc 
+}, {})
+
+const largestInitialObject = Object.assign({}, countLargestInitial (countries))
+const initalCounts = Object.entries(largestInitialObject)
+
+let biggestInitialCharacter = ''
+let biggestCount = 0
+
+for(let i = 0; i < initalCounts.length; i++)
+{
+    if(initalCounts[i][1] > biggestCount)
+    {
+        biggestInitialCharacter = initalCounts[i][0]
+        biggestCount = initalCounts[i][1]
+    }
+}
+console.log(`The biggest character in the countries.js is letter ${biggestInitialCharacter} with a count of ${biggestCount}`)
+
+
+
+
+
+
+
+
+
+
+
+
     
