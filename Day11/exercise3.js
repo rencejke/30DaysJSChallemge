d9841e69a0a987f0202d3844354b99d8df4de525
rencejke <bonillaclarencejake@gmail.com>
2026-08-18 02:00:49 +0800

    day 11 still ongoing

--- Day11/exercise3.js
+++ Day11/exercise3.js
@@ -15,5 +15,46 @@ for(const {name, capital, population, languages} of countries_data)
   let [name, skills, [htmlScore, cssScore, jsScore, reactScore]] = student
   console.log(name, skills, jsScore, reactScore) //David [ 'HTML', 'CSS', 'JS', 'React' ] 90 95
 
+  
+//3 Write a function called convertArrayToObject which can convert the array to a structure object.
+     const students = [
+        ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+        ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+      ]
+const convertArrayToObject = (arr) =>
+{
+
+   const testArr = []
+
+   for(const [name, skill, scores] of arr)
+   {
+        testArr.push({name, skill, scores})
+        
+   }
+
+    return testArr
+
+}
+
+console.log(convertArrayToObject(students))
+
+/*
+
+
+177 ms
+[
+  {
+    name: 'David',
+    skill: [ 'HTM', 'CSS', 'JS', 'React' ],
+    scores: [ 98, 85, 90, 95 ]
+  },
+  {
+    name: 'John',
+    skill: [ 'HTM', 'CSS', 'JS', 'React' ],
+    scores: [ 85, 80, 85, 80 ]
+  }
+]
+ */ 
+
 
 
