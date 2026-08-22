b634580e665cf9abdac2448e32a76a7d5fbc5382
rencejke <bonillaclarencejake@gmail.com>
2026-08-23 02:23:17 +0800

    day 13 started

---
+++ Day13/learning.js
@@ -0,0 +1,17 @@
+//Day 13
+
+//Console Object Methods
+
+//console.log
+
+//We use console.log() to show output on the browser console. 
+
+//1 Showing output on browser console
+
+console.log('30 Days of JavaScript')
+
+//2 Substitution
+
+console.log('%d %s of JavaScript', 30, 'Days')
+
+//CSS
