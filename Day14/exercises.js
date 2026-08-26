3432536c7f8611c91a3083cdd2b71846b5a9475c
rencejke <bonillaclarencejake@gmail.com>
2026-08-27 01:03:58 +0800

    day 14 continued

--- Day14/exercises.js
+++ Day14/exercises.js
@@ -44,8 +44,51 @@ try{
     console.log(err.message) // Cannot read properties of undefined (reading 'toUpperCase')
 }
 
+//3 Number as a String
+//Inside try, attempt to use a string method on age.
 
+try{
+    const age = 25
+    const ageLength = age.substring(0, 1)
+}
+catch(err){
+    console.log(err.name)
+    console.log(err.message)
+}
+finally{
+    console.log("Finished")
+}
 
+//4. Array Operation
+
+//Create an array of numbers.
+
+try{
+    const numbers = [1,2,3,4,5]
+    console.log(numbers.middleIndeOf(2))
+}catch(err)
+{
+    console.log(err.name) //TypeError
+    console.log(err.message) //numbers.middleIndeOf is not a function
+}
+
+//5
+
+try{
+    const numbers = [1,2,3,4,5].indexOf(3)
+    console.log('Everything is working') //Everything is working
+}catch(err){
+    console.log(err.name) 
+    console.log(err.message) 
+}finally{
+    console.log('Process Completed') //Process Completed
+}
+
+//exercises level  2
+
+//1 User Input Validation
+//Create a variable representing a user's age.
+//Inside try, check whether the value can reasonably be used as an age.
 
 
 
