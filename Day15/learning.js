cd2066a31f71aac46109f6081daf0c11b64bd355
rencejke <bonillaclarencejake@gmail.com>
2026-08-30 11:58:25 +0800

    day 15 lessons done

--- Day15/learning.js
+++ Day15/learning.js
@@ -216,6 +216,387 @@ console.log(person9.skill) //[]
 console.log(person10.skill) //[]
 
 
+//getter
+
+// The get method allow us to access value from the object. 
+// We write a get method using keyword get followed by a function.
+// Instead of accessing properties directly from the object we use getter to get the value.
+
+class Person{
+    constructor(firstName, lastName, age, country, city){
+        this.firstName - firstName
+        this.lastName = lastName
+        this.age = age
+        this.country = country,
+        this.score = 0
+        this.skills = []
+    }
+    getFullName()
+    {
+        const fullName = this.firstName + ' ' + this.lastName
+        return fullName
+    }
+    get getScore()
+    {
+        return this.score
+    }
+    get getSkills()
+    {
+        return this.skills 
+    }
+}
+
+const person11 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+const person12 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+
+
+console.log(person11.getScore) //0
+console.log(person11.getScore) //0 
+
+console.log(person12.getSkills) //[]
+console.log(person11.getSkills) //[]
+
+
+//setter
+
+// The setter method allow us to modify the value of certain properties. 
+// We write a setter method using keyword set followed by a function.
+
+
+class Person{
+    constructor(firstName, lastName, age, country, city){
+        this.firstName - firstName
+        this.lastName = lastName
+        this.age = age
+        this.country = country,
+        this.score = 0
+        this.skills = []
+    }
+    getFullName(){
+        const fullName = this.firstName + ' ' + this.lastName
+        return fullName
+    }
+    get getScore(){
+        return this.score
+    }
+    get getSkills(){
+        return this.skills 
+    }
+    set setScore(score){
+        this.score += score
+    }
+    set setSkills(skills){
+        this.skills.push(skills)
+    }
+}
+
+const person13 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+const person14 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+
+person13.setScore = 1
+person13.setSkills = 'HTML'
+person13.setSkills = 'CSS'
+person13.setSkills = 'JavaScript'
+
+person14.setScore = 1
+person14.setSkills = 'Planning'
+person14.setSkills = 'Managing'
+person14.setSkills = 'Organizing'
+
+
+console.log(person13.score) //1
+console.log(person14.score) //1
+
+
+console.log(person13.skills) //[ 'HTML', 'CSS', 'JavaScript' ]
+console.log(person14.skills) //[ 'Planning', 'Managing', 'Organizing' ]
+
+
+class Person{
+    constructor(firstName, lastName, age, country, city){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age,
+        this.country = country,
+        this.city = city,
+        this.score = 0
+        this.skills = []
+    }
+    getFullName(){
+        const fullName = this.firstName + ' ' + this.lastName
+        return fullName
+    }
+    get getScore(){
+        return this.score
+    }
+    get getSkills(){
+        return this.skills 
+    }
+    set setScore(score){
+        this.score += score
+    }
+    set setSkill(skills){
+        this.skills.push(skills)
+    }
+     
+     getPersonInfo(){
+     
+     let fullName = this.getFullName()
+     let skills = 
+        this.skills.length > 0 && 
+        this.skills.slice(0, this.skills.length - 1).join(', ') +
+        ` and ${this.skills[this.skills.length - 1]}`
+     let formattedSkills = skills ? `He knows ${skills}` : ''
+
+     let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
+     return info
+
+     }
+}
+
+
+const person15 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+const person16 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+const person17 = new Person('John', 'Doe', 50, 'Mars', 'Mars city')
+
+person15.setScore = 1
+person15.setSkill = 'HTML'
+person15.setSkill = 'CSS'
+person15.setSkill = 'JavaScript'
+
+person16.setScore = 1
+person16.setSkill = 'Planning'
+person16.setSkill = 'Managing'
+person16.setSkill = 'Organizing'
+
+console.log(person15.getScore)
+console.log(person16.getScore)
+
+console.log(person15.getSkills)
+console.log(person16.getSkills)
+console.log(person17.getSkills)
+
+console.log(person15.getPersonInfo())
+console.log(person16.getPersonInfo())
+console.log(person17.getPersonInfo())
+
+/*
+
+1
+1
+[ 'HTML', 'CSS', 'JavaScript' ]
+[ 'Planning', 'Managing', 'Organizing' ]
+[]
+Asabeneh Yetayeh is 250. He lives Helsinki, Finland. He knows HTML, CSS, and JavaScript
+Lidiya Tekle is 28. He lives Espoo, Finland. He knows Planning, Managing, and Organizing
+John Doe is 50. He lives Mars city, Mars. 
+
+*/
+
+//Static Method
+
+// A static method belongs to the class itself, not its objects (instances).
+// You call it directly using the class name.
+
+//example
+
+//Date.mow()
+
+class Person {
+  constructor(firstName, lastName, age, country, city) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+    this.score = 0
+    this.skills = []
+  }
+  getFullName() {
+    const fullName = this.firstName + ' ' + this.lastName
+    return fullName
+  }
+  get getScore() {
+    return this.score
+  }
+  get getSkills() {
+    return this.skills
+  }
+  set setScore(score) {
+    this.score += score
+  }
+  set setSkill(skill) {
+    this.skills.push(skill)
+  }
+  getPersonInfo() {
+    let fullName = this.getFullName()
+    let skills =
+      this.skills.length > 0 &&
+      this.skills.slice(0, this.skills.length - 1).join(', ') +
+        ` and ${this.skills[this.skills.length - 1]}`
+
+    let formattedSkills = skills ? `He knows ${skills}` : ''
+
+    let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
+    return info
+  }
+    static favoriteSkills() {
+        const skills = ['HTML', 'CSS', 'JS', 'React', 'Python', 'Node']
+        const index = Math.floor(Math.random() * skills.length)
+
+        return skills[index] 
+    }
+
+    static showDateTime(){
+        let now = new Date()
+        let year = now.getFullYear()
+        let month = now.getMonth() + 1
+        let date = now.getDate()
+        let hours = now.getHours()
+        let minutes = now.getMinutes()
+
+        if(hours < 10){
+            hours = '0' + hours
+        }
+
+        if(minutes < 10){
+            minutes = '0' + minutes
+        }
+
+        let formattedDate = date + '.' + month + '.' + year
+        let time  = hours + ':' + minutes
+        let fullTime = formattedDate + ' ' + time
+
+        return fullTime
+
+    }
+}
+
+
+console.log(Person.favoriteSkills()) //HTML
+console.log(Person.showDateTime()) //30.8.2026 03:20
+
+//Inheritance
+
+//Inheritance allows a child class to use the properties and methods of a parent class.
+//It helps reduce repeated code.
+//For example, Person can be the parent class, while Student and Teacher can be child classes.
+
+// syntax 
+
+class childClassName extends parentClassName{
+    //code here 
+} 
+
+class Student extends Person{
+    saySomething() {
+        return 'I am a child of the person class'
+    }
+}
+
+const stud1 =  new Student('Asabeneh', 'Yetayeh', 'Finland', 250, 'Helsinki')
+console.log(stud1)
+console.log(stud1.saySomething())
+console.log(stud1.getFullName())
+console.log(stud1.getPersonInfo())
+
+
+/*
+
+Student {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 'Finland',
+  country: 250,
+  city: 'Helsinki',
+  score: 0,
+  skills: []
+}
+I am a child of the person class
+Asabeneh Yetayeh
+Asabeneh Yetayeh is Finland. He lives Helsinki, 250. 
+
+*/
+
+class Student extends Person{
+    constructor(firstName, lastName, age, country, city, gender){
+        super(firstName, lastName, age, country, city)
+        this.age = age 
+    }
+
+    saySomething() {
+        return 'I am a child of the person class'
+    }
+
+    getPersonInfo() {
+    let fullName = this.getFullName()
+    let skills =
+      this.skills.length > 0 &&
+      this.skills.slice(0, this.skills.length - 1).join(', ') +
+        ` and ${this.skills[this.skills.length - 1]}`
+
+    let formattedSkills = skills ? `He knows ${skills}` : ''
+    let pronoun = this.gender === 'Male' ? 'He' : 'She'
+
+    let info = `${fullName} is ${this.age}. ${pronoun} lives ${this.city}, ${this.country}. ${formattedSkills}`
+    return info
+  }
+}
+
+const stud2 = new Student(
+  'Asabeneh',
+  'Yetayeh',
+  250,
+  'Finland',
+  'Helsinki',
+  'Male'
+  )
+
+const stud3 = new Student('Lidiya', 'Tekle', 28, 'Finland', 'Helsinki', 'Female')
+
+stud2.setScore = 1
+stud2.setSkill = 'HTML'
+stud2.setSkill = 'CSS'
+stud2.setSkill = 'JavaScript'
+
+stud3.setScore = 1
+stud3.setSkill = 'Planning'
+stud3.setSkill = 'Managing'
+stud3.setSkill = 'Organizing'
+
+console.log(stud2)
+
+console.log(stud2.saySomething())
+console.log(stud2.getFullName())
+console.log(stud2.getPersonInfo())
+
+
+console.log(stud3.saySomething())
+console.log(stud3.getFullName())
+console.log(stud3.getPersonInfo())
+
+
+/*
+
+Student {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  score: 1,
+  skills: [ 'HTML', 'CSS', 'JavaScript' ]
+}
+I am a child of the person class
+Asabeneh Yetayeh
+Asabeneh Yetayeh is 250. She lives Helsinki, Finland. He knows HTML, CSS and JavaScript
+I am a child of the person class
+Lidiya Tekle
+Lidiya Tekle is 28. She lives Helsinki, Finland. He knows Planning, Managing and Organizing
+
+*/
+
+
 
 
 
