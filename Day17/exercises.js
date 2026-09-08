70197d58ae48d05944c377feb5e4777b13d7e3bf
rencejke <bonillaclarencejake@gmail.com>
2026-09-09 00:27:58 +0800

    exercises started

---
+++ Day17/exercises.js
@@ -0,0 +1,24 @@
+//Exercises: Level 1
+
+//Store you first name, last name, age, country, city in your browser localStorage.
+
+let firstName = 'kim'
+let lastName = 'chaewon'
+let age = 26
+let country = 'Korea'
+let city = 'Seoul'
+ 
+localStorage.setItem('firtsName', firstName)
+localStorage.setItem('lastName', lastName)
+localStorage.setItem('age', age)
+localStorage.setItem('country', country)
+localStorage.setItem('city', city)
+console.log(localStorage)
+
+
+
+
+
+
+
+
